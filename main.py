372188872249507cbee2e93c27498a08b2317c1d
sameer borkar <sameer@sameers-MacBook-Air.local>
2021-10-03 21:53:54 +0530

    deployment issue fix

--- main.py
+++ main.py
@@ -122,5 +122,5 @@ def get_data_object(date_str):
   return datetime_obj
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
